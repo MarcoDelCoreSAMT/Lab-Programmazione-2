86360b9c8cd0e50dab7b57a61c634249b4a8cd17
MarcoDelCoreCIAO <ibv606@student.edu.ti.ch>
2026-02-25 14:39:00 +0100

    Continuazione quiz
    25.02.2026

--- AppQuiz/MainPage.xaml.cs
+++ AppQuiz/MainPage.xaml.cs
@@ -11,13 +11,14 @@ namespace AppQuiz
         public MainPage()
         {
             InitializeComponent();
-            _questions.Add(new TrueFalseQuestion("C# è un linguaggio ad oggetti?", 10, true, "https://it.wikipedia.org/wiki/File:C_Sharp_Logo_2023.svg"));
-            _questions.Add(new TrueFalseQuestion("Python è un linguaggio compilato?", 15, false, "https://www.python.org/community/logos/"));
+            _questions.Add(new TrueFalseQuestion("C# è un linguaggio ad oggetti?", 10, true, "csharp_logo.png"));
+            _questions.Add(new TrueFalseQuestion("Python è un linguaggio compilato?", 15, false, "python_symbol.png"));
             ShowQuestion();
+            btnResult.IsEnabled = false;
         }
 
         // L'evento porta con se il sender che è il button
-        private void OnAnswer_Clicked(object sender, EventArgs e)
+        private async void OnAnswer_Clicked(object sender, EventArgs e)
         {
             // Downcast a bottone, controlla se è True o False 
             Button btn = (Button)sender;
@@ -26,10 +27,10 @@ namespace AppQuiz
             if (_questions[_currentIndex].CheckAnswer(userAnswer))
             {
                 _score += _questions[_currentIndex].Points;
-                DisplayAlert("Giusto!", "Bravo hai risposto correttamente!.", "GODO");
+                await DisplayAlert("Giusto!", "Bravo hai risposto correttamente!.", "GODO");
             }
             else {
-                DisplayAlert("Errato!", "Risposta sbagliata...", "Accidenti...");
+                await DisplayAlert("Errato!", "Risposta sbagliata...", "Accidenti...");
             }
                 _currentIndex++;
             ShowQuestion();
@@ -48,12 +49,26 @@ namespace AppQuiz
             }
             else
             {
-                QuestionTextLabel.Text = $"Quiz completato!\nPunteggio finale: {_score}/25";
-                QuestionImage.Source = "https://tenor.com/search/ok-emoji-gifs";
+                QuestionImage.Source = "fineciao.png";
                 TrueButton.IsEnabled = FalseButton.IsEnabled = false;
+                QuestionImage.IsEnabled = false;
+                QuestionTextLabel.Text = "Fine";
+                ScoreLabel.Text = "";
+                btnResult.IsEnabled = true;
             }
         }
 
+        private void btnResult_Clicked(object sender, EventArgs e)
+        {
+            OnQuizFinished();
+        }
+
+        private async void OnQuizFinished() 
+        {
+            // Richiamiamo il metodo PushAsync e gli passiamo il nuovo oggetto ResultPage
+            // Attendiamo senza bloccare la pagina grazie ad await e async
+            await Navigation.PushAsync(new ResultPage(_score));
+        }
     }
 
 }
---
+++ AppQuiz/ResultPage.xaml.cs
@@ -0,0 +1,20 @@
+namespace AppQuiz;
+
+public partial class ResultPage : ContentPage
+{
+	int _score = 0;
+	public ResultPage(int score)
+	{
+		// la variabile nasce e muore nel costruttore, da assegnare o da rendere globale
+		_score = score;
+		InitializeComponent();
+		ShowGUI();
+	}
+
+	// variabile globalizzata
+	private void ShowGUI() 
+	{
+		lblScore.Text = _score.ToString();
+		champ.SourceImage = "";
+	}
+}
