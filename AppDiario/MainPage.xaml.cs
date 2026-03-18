7e60a21b7ecb05995557d64932efde98989faa56
MarcoDelCoreCIAO <ibv606@student.edu.ti.ch>
2026-03-18 15:42:27 +0100

    AppDiario inizio
    18.03.2026

---
+++ AppDiario/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using AppDiario.Models;
+
+namespace AppDiario
+{
+    public partial class MainPage : ContentPage
+    {
+        // Percorso del file dove salvare le note - calcolato una sola volta
+        // %appdata% local package com.companyAppDiario LocalState note.txt
+        string percorsoFile = Path.Combine(
+            FileSystem.AppDataDirectory,
+            "note.txt"
+        );
+
+        public MainPage()
+        {
+            InitializeComponent(); // Carica lo XAML
+        }
+
+        private async void OnSalvaClicked(object sender, EventArgs e)
+        {
+            Nota nuovaNota = new Nota();
+
+            nuovaNota.Titolo = EntTitolo.Text;
+            nuovaNota.Testo = EntTesto.Text;
+
+            if (string.IsNullOrEmpty(nuovaNota.Titolo))
+            {
+                await DisplayAlert("NO!",
+                    "Dai per favore metti almeno il titolo", "Eh ok");
+                return; // interrompe il metodo
+            }
+
+            // Serializza oggetto --> string CSV
+            string rigaDaScrivere = nuovaNota.daOggettoARiga();
+
+            // Scrive sul file (aggiunge in fondo)
+            File.AppendAllText( percorsoFile,
+                rigaDaScrivere + Environment.NewLine);
+
+            // Pulisce i campi di input
+            EntTitolo.Text = "";
+            EntTesto.Text = "";
+
+            // Conferma 
+            await DisplayAlert("Bom!", "l'hai salvata bene!", "garzie");
+        }
+
+        private void OnLeggiClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ AppDiario/Models/Nota.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDiario.Models
+{
+    // La classe contiene un titolo e un testo
+    internal class Nota
+    {
+		private string _titolo;
+
+		public string Titolo
+		{
+			get { return _titolo; }
+			set 
+			{
+				if (String.IsNullOrEmpty(value)) 
+				{ 
+					value = "sconosciuto";
+                }
+				_titolo = value;
+			}
+		}
+
+        private string _testo;
+
+        public string Testo
+		{
+			get { return _testo; }
+			set { _testo = value; }
+		}
+
+        public string daOggettoARiga()
+        {
+            return this.Titolo + ";" + this.Testo;
+        }
+
+		// ritorna un oggetto Nota
+		public static Nota daRigaAOggetto(string riga)
+		{
+			string[] parti = riga.Split(';');
+			if (parti.Length < 2) { return null; }
+
+			Nota nuovaNota = new Nota();
+			nuovaNota.Titolo = parti[0]; // La prima parte è il titolo
+			nuovaNota.Testo = parti[1]; // La seconda parte è il testo
+			return nuovaNota;
+		}
+	}
+}
