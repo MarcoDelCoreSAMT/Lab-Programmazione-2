7a0182acca6f8d3f8417137fd2e344d3ebdcf1ca
MarcoDelCoreCIAO <ibv606@student.edu.ti.ch>
2026-04-01 15:37:43 +0200

    HelloList
    01.04.2026

---
+++ HelloList/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace HelloList
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> frutti; 
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ShowGUI();
+        }
+
+        private void ShowGUI()
+        {
+            frutti = new List<string>();
+            frutti.Add("Mela");
+            frutti.Add("Pera");
+            frutti.Add("Banana");
+            pickFrutti.ItemsSource = frutti;
+        }
+    }
+
+}
